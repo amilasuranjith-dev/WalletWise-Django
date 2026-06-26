8fd6274515b348cebd4ea15e371c12540fc221b0
amilasuranjith-dev <amilasuranjith.dev@gmail.com>
2026-06-26 07:33:20 +0530

    feat:  CustomUser in Unfold admin theme

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from unfold.admin import ModelAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser, UserAdmin)
+@admin.register(CustomUser)
+class CustomUserAdmin(BaseUserAdmin, ModelAdmin):
+    pass
